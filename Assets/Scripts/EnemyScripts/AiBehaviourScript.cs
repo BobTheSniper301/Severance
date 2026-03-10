a5714722d158175e9262bda7c9d76ab4a6f965b4
presbish09 <PrestonBishop500@gmail.com>
2026-03-10 12:34:28 -0500

    Enemy
    slight change

--- Assets/Scripts/EnemyScripts/AiBehaviourScript.cs
+++ Assets/Scripts/EnemyScripts/AiBehaviourScript.cs
@@ -5,10 +5,13 @@ public class AiBehaviourScript : MonoBehaviour
     public bool sleepState = false;
     public bool playerSeen = false;
 
+    Transform[] Patrols;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Patrols = GameObject.Find("Patrols").GetComponentsInChildren<Transform>();
+        Patrols.
     }
 
     // Update is called once per frame
